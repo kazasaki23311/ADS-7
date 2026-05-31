c381d00159f7bb7d7bd64bc88fdc625dba732665
kazasaki23311 <julia.nikashina@gmail.com>
2026-05-31 14:50:32 +0300

    Никашина адс7 мэйн

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,54 @@
 // Copyright 2022 NNTU-CS
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
+#include <iomanip>
 #include "train.h"
 
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
-
-  while (count--)
-    train.addCar(false);
-
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
+  std::srand(time(nullptr));std::cout << std::setw(10) << "n"
+    << std::setw(15) << "all false"
+    << std::setw(15) << "all true"
+    << std::setw(15) << "random"
+    << std::setw(12) << "n^2"
+    << std::endl;
+  std::cout << std::string(67, '-') << std::endl;
+  for (int n = 10; n <= 200; n += 10) {
+    long long opFalse = 0;
+    long long opTrue = 0;
+    long long opRandom = 0;
+    {
+      Train train;
+      for (int i = 0; i < n; i++)
+        train.addCar(false);
+      train.getLength();
+      opFalse = train.getOpCount();
+      }
+    {
+      Train train;
+      for (int i = 0; i < n; i++)
+        train.addCar(true);
+      train.getLength();
+      opTrue = train.getOpCount();
+      }
+    {
+      long long totalRandom = 0;
+      for (int trial = 0; trial < 5; trial++) {
+        Train train;
+        for (int i = 0; i < n; i++)
+          train.addCar(std::rand() % 2);
+        train.getLength();
+        totalRandom += train.getOpCount();
+        }
+      opRandom = totalRandom / 5;
+      }
+    long long nSquared = n * n;
+    std::cout << std::setw(10) << n
+      << std::setw(15) << opFalse
+      << std::setw(15) << opTrue
+      << std::setw(15) << opRandom
+      << std::setw(12) << nSquared
+      << std::endl;
+    }
   return 0;
 }
